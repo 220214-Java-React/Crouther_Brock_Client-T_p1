c46886757824a4a4c388e5400c8f5074c604c05c
mbrock76 <mbrock76@mail.com>
2022-03-22 20:23:09 -0700

    styled landing page

--- script/login.js
+++ script/login.js
@@ -10,6 +10,13 @@ function init(){
 		if(window.innerWidth < window.innerHeight){
 			document.getElementById("css").href = "css/login_vrt.css";
 			isMobile = true;
+			
+			document.getElementById("title").innerHTML = 'Reimbursement</br>&nbsp;&nbsp;&nbsp;Application';
+		}else{
+			document.getElementById("css").href = "css/login_hrz.css";
+			isMobile = false;
+			
+			document.getElementById("title").innerHTML = 'Reimbursement Application';
 		}
 	}
 }
