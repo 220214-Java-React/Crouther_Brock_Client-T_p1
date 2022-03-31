262dace4cf9c1823d957f2cf6a0c7d1f37f5f59d
mkcrouther@gmail.com <33792608+crouther@users.noreply.github.com>
2022-03-31 11:49:03 -0400

    Table Enhancements: JavaScript Web Component Modifications

---
+++ script/Reimbursement.js
@@ -0,0 +1,81 @@
+class Reimbursement {
+
+	Reimbursement_ID = 0;
+	Amount = 0;
+	Submitted = "";
+	Resolved = "";
+	Description = "Details Pending...";
+	Recipt = "";
+	Payment_ID = "";
+	Author_ID = "Employee";
+	Resolver_ID = "Revature";
+	Status_ID = "Pending";
+	Type_ID = "Other";
+
+	constructor(rei_ID, a, s, res, des, rec, P_ID, A_ID, R_ID, S_ID, T_ID){
+		this.Reimbursement_ID = rei_ID;
+		this.Amount = a;
+		this.Submitted = s;
+		this.Resolved = res,
+		this.Description = des;
+		this.Recipt = rec;
+		this.Payment_ID = P_ID;
+		this.Author_ID = A_ID;
+		this.Resolver_ID = R_ID;
+		this.Status_ID = S_ID;
+		this.Type_ID = T_ID;
+	}
+
+	function addReimbursement(rei){
+		let row = document.createElement('tr');
+		row.setAttribute('id', rei.Reimbursement_ID);
+
+		if(document.getElementById("TAB").childElementCount%2 == 0) row.setAttribute('class', "white");
+
+		let r_id = document.createElement('td');
+		r_id.textContent = rei.Reimbursement_ID;
+
+		let Amount = document.createElement('td');
+		Amount.textContent = rei.Amount;
+
+		let Submitted = document.createElement('td');
+		Submitted.textContent = rei.Submitted;
+
+		let Resolved = document.createElement('td');
+		Resolved.textContent = rei.Resolved;
+
+		let Description = document.createElement('td');
+		Description.textContent = rei.Description;
+
+		let Recipt = document.createElement('td');
+		Recipt.textContent = rei.Recipt;
+
+		let Payment_ID = document.createElement('td');
+		Payment_ID.textContent = rei.Payment_ID;
+
+		let Author_ID = document.createElement('td');
+		Author_ID.textContent = rei.Author_ID;
+
+		let Resolver_ID = document.createElement('td');
+		Resolver_ID.textContent = rei.Resolver_ID;
+
+		let Status_ID = document.createElement('td');
+		Status_ID.textContent = rei.Status_ID;
+
+		let Type_ID = document.createElement('td');
+		Type_ID.textContent = rei.Type_ID;
+
+		document.getElementById("TAB").appendChild(row);
+		document.getElementById(rei.Reimbursement_ID).appendChild(r_id);
+		document.getElementById(rei.Reimbursement_ID).appendChild(Amount);
+		document.getElementById(rei.Reimbursement_ID).appendChild(Submitted);
+		document.getElementById(rei.Reimbursement_ID).appendChild(Resolved);
+		document.getElementById(rei.Reimbursement_ID).appendChild(Description);
+		document.getElementById(rei.Reimbursement_ID).appendChild(Recipt);
+		document.getElementById(rei.Reimbursement_ID).appendChild(Payment_ID);
+		document.getElementById(rei.Reimbursement_ID).appendChild(Author_ID);
+		document.getElementById(rei.Reimbursement_ID).appendChild(Resolver_ID);
+		document.getElementById(rei.Reimbursement_ID).appendChild(Status_ID);
+		document.getElementById(rei.Reimbursement_ID).appendChild(Type_ID);
+	}
+}
